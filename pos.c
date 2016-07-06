e91f1b2280915d7f2c5333c332065598ba941ecc
piriyanka <piriyankajegan@gmail.com>
2016-07-06 14:35:23 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=1;
+if(a>0)
+{
+printf("positive number");
+}
+else if(a<0)
+{
+printf("Negative number");
+}
+else
+{
+printf("The number is zero");
+}
+getch();
+}
