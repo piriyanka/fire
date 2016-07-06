2385869dce2f98a49721c54678c321ccfad9a5eb
piriyanka <piriyankajegan@gmail.com>
2016-07-06 14:58:53 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n=4;
+for(i=2;i<=n;i++)
+if(n%i==1)
+{
+count=1;
+}
+else
+{
+count=0;
+}
+if(count==0)
+{
+printf("Prime number");
+}
+getch();
+}
