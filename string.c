8a25f2e36b85f033e3147a8da2a69c9f1fb549d3
piriyanka <piriyankajegan@gmail.com>
2016-09-30 13:58:59 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],n,i;
+printf("Enter the value");
+scanf("%d",&n);
+for(i=0;i<a[i];i++)
+{
+if(i==a[3]||i==a[7]||i==a[9])
+printf("a %d",a[i]);
+{
+a[i]='.';
+i++;
+printf(".");
+}
+printf("%d",a);
+}
+getch();
+}
